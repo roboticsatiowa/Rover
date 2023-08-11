1a6a99c02c4554178c5c659dfab2911aea6790e2
Ethan Holter <holterethan@gmail.com>
2023-08-10 23:00:23 -0500

    run video test script

---
+++ run_video.py
@@ -0,0 +1,23 @@
+import base64
+import pickle
+import socket
+import struct
+from time import sleep
+
+import cv2
+import imutils
+import pyperclip
+
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 42069  # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as conn:
+    conn.connect((HOST, PORT))
+    print(f"Connected to {HOST} on port {PORT}...")
+    vid = cv2.VideoCapture(0)
+    while vid.isOpened():
+        img, frame = vid.read()
+        frame = imutils.resize(frame, width=320)
+        a = pickle.dumps(frame)
+        message = struct.pack("Q", len(a)) + a
+        conn.sendall(message)
