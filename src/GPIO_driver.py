ac639c2703c68dc401021dfba9181f993d8af519
Ethan Holter <holterethan@gmail.com>
2023-08-11 17:34:25 -0500

    adjusted arm calibration

--- src/GPIO_driver.py
+++ src/GPIO_driver.py
@@ -74,13 +74,13 @@ def handleInput(input:str):
     
     if L[0] == 'RELEASED':
         if L[1] == 'A':
-            pca.channels[14].duty_cycle = 32767
+            pca.channels[14].duty_cycle = 30000
         if L[1] == 'X':
-            pca.channels[14].duty_cycle = 32767
+            pca.channels[14].duty_cycle = 30000
         if L[1] == 'Y':
-            pca.channels[15].duty_cycle = 32767
+            pca.channels[15].duty_cycle = 30000
         if L[1] == 'B':
-            pca.channels[15].duty_cycle = 32767
+            pca.channels[15].duty_cycle = 30000
             
 def disable():
     pca.channels[12].duty_cycle = 0
