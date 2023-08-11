bff41e70b8445cd53ced911dc23ce0efe6cf358d
Ethan Holter <holterethan@gmail.com>
2023-08-11 10:30:15 -0500

    made hardcoded values cli args

--- run_video.py
+++ run_video.py
@@ -1,20 +1,29 @@
-import base64
 import pickle
 import socket
 import struct
-from time import sleep
+import sys
 
 import cv2
-import imutils
-import pyperclip
 
-HOST = "192.168.1.13"  # The server's hostname or IP address
-PORT = 55555  # The port used by the server
+try:
+    HOST = sys.argv[1]
+except IndexError:
+    HOST = input("IPv4: ") # 192.168.1.13
+
+try: 
+    PORT = int(sys.argv[2])
+except IndexError:
+    PORT = int(input("PORT: "))
+
+try: 
+    cam_index = int(sys.argv[3])
+except IndexError:
+    cam_index = int(input("Camera index: "))
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as conn:
     conn.connect((HOST, PORT))
     print(f"Connected to {HOST} on port {PORT}...")
-    vid = cv2.VideoCapture(0)
+    vid = cv2.VideoCapture(cam_index)
     while vid.isOpened():
         img, frame = vid.read()
         #frame = imutils.resize(frame, width=320)
