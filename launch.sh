3680fb854029e3cff9ee9fa91ee5179b6190d24f
Ethan Holter <holterethan@gmail.com>
2023-08-13 11:59:23 -0500

    changed laptop ip

--- launch.sh
+++ launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python3 run.py 192.168.137.1 55559 &
+python3 run.py 192.168.1.13 55559 &
 
-python3 run_video.py 192.168.137.1 55555 0 &
-python3 run_video.py 192.168.137.1 55556 2 &
-python3 run_video.py 192.168.137.1 55558 4 &
+python3 run_video.py 192.168.1.13 55555 0 &
+python3 run_video.py 192.168.1.13 55556 2 &
+python3 run_video.py 192.168.1.13 55558 4 &
