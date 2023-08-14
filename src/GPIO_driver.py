9fdab14f10f09916d8f71d5a6025de2bb703490b
Ethan Holter <holterethan@gmail.com>
2023-08-14 11:29:38 -0500

    fix

--- src/GPIO_driver.py
+++ src/GPIO_driver.py
@@ -68,11 +68,11 @@ def handle_drive_input(L):
         if L[1] == 'LY':
             if val > THRESHHOLD:
                 Right_duty_cycle = lerp(val,0,1,SOFT_LOW,SOFT_HIGH)
-                GPIO.output(RIGHT_DIR, True)
+                GPIO.output(RIGHT_DIR, False)
                 right_pwm.ChangeDutyCycle(Right_duty_cycle)
             elif val < -THRESHHOLD:
                 Right_duty_cycle = lerp(val,0,-1,SOFT_LOW,SOFT_HIGH)
-                GPIO.output(RIGHT_DIR, False)
+                GPIO.output(RIGHT_DIR, True)
                 right_pwm.ChangeDutyCycle(Right_duty_cycle)
             else:
                 GPIO.output(RIGHT_DIR, True)
