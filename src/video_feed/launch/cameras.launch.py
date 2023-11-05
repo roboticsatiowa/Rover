655f3b74b2d2aa9ecb90a46595e527f4ea71f354
Charlie <killiancharlie2@gmail.com>
2023-11-05 14:29:57 -0600

    runs using rqt

--- src/video_feed/launch/cameras.launch.py
+++ src/video_feed/launch/cameras.launch.py
@@ -1,25 +1,27 @@
 import os
-
-import launch
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration
-
-def generate_camera_info_url(camera_name):
-    ros_home = os.environ.get('ROS_HOME', '/home/robotics/.ros')
-    return f'file://{ros_home}/camera_info/{camera_name}.yaml'
+from launch.substitutions import LaunchConfiguration, Command
 
 def generate_launch_description():
+    # Use the 'ROS_HOME' environment variable if it's set, otherwise default to '/home/robotics/.ros'
+    default_ros_home = os.environ.get('ROS_HOME', '/home/robotics/.ros')
+    
+    # Configure the camera_info_url to dynamically create the path to the camera calibration file
+    camera_info_url = Command([
+        'echo "file://', default_ros_home, '/camera_info/', LaunchConfiguration('camera_name'), '.yaml"'
+    ])
+    
     return LaunchDescription([
         DeclareLaunchArgument(
             'camera_name',
-            default_value='camera',
+            default_value='default_cam',  # Set the default camera name to 'default_cam'
             description='Name of the camera node'
         ),
         DeclareLaunchArgument(
             'video_device',
-            default_value='/dev/video0',
+            default_value='/dev/video0',  # Default to using '/dev/video0' for the webcam
             description='Video device path of the USB webcam'
         ),
         Node(
@@ -29,7 +31,9 @@ def generate_launch_description():
             namespace='camera',
             parameters=[{
                 'video_device': LaunchConfiguration('video_device'),
-                'camera_info_url': generate_camera_info_url(LaunchConfiguration('camera_name')),
+                'camera_info_url': camera_info_url,  # Use the dynamically constructed camera_info_url
+                'camera_name': LaunchConfiguration('camera_name'),  # Set the camera name
+                # Additional parameters can be added here if needed
             }],
             remappings=[('image_raw', 'video_feed')],
         ),
