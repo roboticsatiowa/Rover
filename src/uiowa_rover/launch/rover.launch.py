27f21adc5261ac83e001f9f564120fc2accc49ac
Ethan <holterethan@gmail.com>
2023-11-15 21:08:58 -0600

    improved main launch file

--- src/uiowa_rover/launch/rover.launch.py
+++ src/uiowa_rover/launch/rover.launch.py
@@ -10,20 +10,47 @@ from launch.actions import ExecuteProcess
 def generate_launch_description():
     # include launch file in video_feed/launch/video_feed.launch.py
 
+    rover_ip = "192.168.1.TODO"
+    rover_user = "robotics"
+    rover_launch_cmd = "echo 'TODO'"
+    tmux_session_name = "ros"
+
+    # Start video feeds
     launch_video_feeds = IncludeLaunchDescription(
         AnyLaunchDescriptionSource(
             PathJoinSubstitution(
-                [FindPackageShare("video_feed"),"launch/cams.launch.xml"]
+                [FindPackageShare("video_feed"), "launch/cams.launch.xml"]
             )
         )
     )
 
-    node_foxglove_bridge = Node(name="foxglove_bridge", package="foxglove_bridge", executable="foxglove_bridge", output="screen")
+    # Start foxglove_bridge node
+    node_foxglove_bridge = Node(
+        name="foxglove_bridge",
+        package="foxglove_bridge",
+        executable="foxglove_bridge",
+        output="screen",
+    )
 
+    # Start foxglove studio
     exec_foxglove_studio = ExecuteProcess(cmd=["foxglove-studio"])
 
-    return LaunchDescription([
-        node_foxglove_bridge,
-        exec_foxglove_studio,
-        launch_video_feeds,
-    ])
+    # SSh into rover and start tmux session if it doesn't exist - create it and run launch command then exit
+    exec_start_rover = ExecuteProcess(
+        cmd=[
+            f"\
+    ssh {rover_user}@{rover_ip} \
+    'if ! tmux has-session -t {tmux_session_name}; \
+        then tmux new-session -d -s {tmux_session_name} \"{rover_launch_cmd}\"; \
+    fi'; \
+    exit"
+        ]
+    )
+    return LaunchDescription(
+        [
+            node_foxglove_bridge,
+            exec_foxglove_studio,
+            launch_video_feeds,
+            exec_start_rover,
+        ]
+    )
