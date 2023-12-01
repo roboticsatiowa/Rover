ba53735e6504d6299bf042552be102263c5c5d9a
Charlie <killiancharlie2@gmail.com>
2023-11-30 19:24:09 -0600

    initial commit

--- src/aruco_detection/setup.py
+++ src/aruco_detection/setup.py
@@ -19,7 +19,8 @@ setup(
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
+        'console_scripts': [ 
+            'aruco_detector_node = aruco_detector.aruco_detector_node:main'
         ],
     },
 )
