44479e0b8e9d0f9ee2d04fe9bc6b3635d0d8d723
Ethan Holter <holterethan@gmail.com>
2024-02-08 19:56:03 -0600

    fixed issue

--- src/simple_hardware_interface/simple_hardware_interface/interface_node.py
+++ src/simple_hardware_interface/simple_hardware_interface/interface_node.py
@@ -42,12 +42,10 @@ class InterfaceNode(Node):
         
         axes = msg.axes
         buttons = msg.buttons
-        
-        if axes[1] != 0:
-            self.serial_out.write(b'h 0 ' + bytes(str(axes[1] * 255), 'utf-8') + b'\r')
-        if axes[4] != 0:
-            self.serial_out.write(b'h 0 ' + bytes(str(axes[4] * 255), 'utf-8') + b'\r')
-
+    
+        self.serial_out.write(b'h 0 ' + bytes(str(axes[1] * 255), 'utf-8') + b'\r')
+        self.serial_out.write(b'h 1 ' + bytes(str(axes[4] * 255), 'utf-8') + b'\r')
+        sleep(0.01)
         
 
 
