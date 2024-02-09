c28221bf01d4251196e82179103aeb9f60e74233
Ethan Holter <holterethan@gmail.com>
2024-02-08 18:52:17 -0600

    stuff

--- src/simple_hardware_interface/simple_hardware_interface/interface_node.py
+++ src/simple_hardware_interface/simple_hardware_interface/interface_node.py
@@ -1,26 +1,67 @@
 import rclpy
 from rclpy.node import Node
+from sensor_msgs.msg import Joy
+import serial
+from time import sleep
 
 class InterfaceNode(Node):
     def __init__(self):
-        super().__init__('simple_hardware_interface')
+        super(InterfaceNode, self).__init__('simple_hardware_interface')
+
+        #NOTE port name may need to be changed
+
+        PORT_NAME = '/dev/ttyACM0'
+        
+        attempts_remaining = 5
+        while attempts_remaining > 0:
+            attempts_remaining -= 1
+            try:
+                self.serial_out = serial.Serial(PORT_NAME, 9600, timeout=1)
+                break
+            except:
+                self.get_logger().warn(f"Failed to open serial port {PORT_NAME}. Retrying...")
+                sleep(5)
+
+        if attempts_remaining == 0:
+            self.get_logger().error("Maximum attempts reached. Exiting node")
+            self.destroy_node()
+
+
+        self.control_mode = 0
 
         self.joystick_subscription = self.create_subscription(
             Joy,
             'joy',
             self.joystick_callback,
             10)
+    
+        
+
         
     def joystick_callback(self, msg):
-        rclpy.loginfo('Joystick message received: ' + msg)
-    
+        
+        axes = msg.axes
+        buttons = msg.buttons
+
+        self.get_logger().info("Axes: " + str(axes) +'\n')
+
+        if axes[1] != 0:
+            self.serial_out.write(b'h 0 ' + axes[1] * 255)
+        if axes[4] != 0:
+            self.serial_out.write(b'h 0 ' + axes[4] * 255)
+
+        
+
+
+
 
 
 def main(args=None):
     rclpy.init(args=args)
-    node = Node()
+    #TODO send startup message to teensy
+    node = InterfaceNode()
     rclpy.spin(node)
+    #TODO send shutdown message to teensy
     rclpy.shutdown()
-
 if __name__ == '__main__':
     main()
