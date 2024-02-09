7d66797ae24cb08df594cd96d0dc6d4761ae6ec0
Ethan Holter <holterethan@gmail.com>
2024-02-08 19:52:29 -0600

    removed log statements

--- src/simple_hardware_interface/simple_hardware_interface/interface_node.py
+++ src/simple_hardware_interface/simple_hardware_interface/interface_node.py
@@ -43,10 +43,8 @@ class InterfaceNode(Node):
         axes = msg.axes
         buttons = msg.buttons
         
-        self.get_logger().info(str(axes[1]))
         if axes[1] != 0:
             self.serial_out.write(b'h 0 ' + bytes(str(axes[1] * 255), 'utf-8') + b'\r')
-            self.get_logger().info('h 0 ' + bytes(str(axes[1] * 255), 'utf-8') + b'\r')
         if axes[4] != 0:
             self.serial_out.write(b'h 0 ' + bytes(str(axes[4] * 255), 'utf-8') + b'\r')
 
