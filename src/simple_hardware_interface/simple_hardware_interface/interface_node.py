02d79296a49cd9c82e703eb2071ff7da5f2fd587
Ethan Holter <holterethan@gmail.com>
2024-02-08 19:16:07 -0600

    added carriage return

--- src/simple_hardware_interface/simple_hardware_interface/interface_node.py
+++ src/simple_hardware_interface/simple_hardware_interface/interface_node.py
@@ -46,9 +46,9 @@ class InterfaceNode(Node):
         self.get_logger().info("Axes: " + str(axes) +'\n')
 
         if axes[1] != 0:
-            self.serial_out.write(b'h 0 ' + axes[1] * 255)
+            self.serial_out.write(b'h 0 ' + axes[1] * 255) + '\r'
         if axes[4] != 0:
-            self.serial_out.write(b'h 0 ' + axes[4] * 255)
+            self.serial_out.write(b'h 0 ' + axes[4] * 255 + '\r')
 
         
 
