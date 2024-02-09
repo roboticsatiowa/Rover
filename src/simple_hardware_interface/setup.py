cc451a25e8fa4c68bad263c1a637d30bd92e790b
Ethan Holter <holterethan@gmail.com>
2024-02-08 18:52:17 -0600

    stuff

--- src/simple_hardware_interface/setup.py
+++ src/simple_hardware_interface/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            "simple_hardware_interface = simple_hardware_interface.interface_node:main"
         ],
     },
 )
