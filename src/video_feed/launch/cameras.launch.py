341a24884b6fae3266321fd7f407c3c7f81de24e
Charlie <killiancharlie2@gmail.com>
2024-02-11 14:10:05 -0600

    mjpeg pixel format

--- src/video_feed/launch/cameras.launch.py
+++ src/video_feed/launch/cameras.launch.py
@@ -8,12 +8,13 @@ def generate_launch_description():
     # Get the directory of the current file (i.e., the directory where this launch file is located)
     launch_file_dir = os.path.dirname(os.path.realpath(__file__))
     
+    
     # Initialize LaunchDescription list
     ld = []
-
+    pixel_format = 'mjpeg2rgb'
     # Camera 1
     camera_name_1 = 'camera1'
-    video_device_1 = '/dev/video2'
+    video_device_1 = '/dev/video0'
     if os.path.exists(video_device_1):
         camera_info_url_1 = Command([
             'echo "file://', launch_file_dir, '/', camera_name_1, '.yaml"'
@@ -27,13 +28,15 @@ def generate_launch_description():
                 'video_device': video_device_1,
                 'camera_info_url': camera_info_url_1,
                 'camera_name': camera_name_1,
+                'pixel_format': pixel_format
             }],
             remappings=[('image_raw', 'image_raw')]
         ))
 
     # Camera 2
     camera_name_2 = 'camera2'
-    video_device_2 = '/dev/video0'
+    video_device_2 = '/dev/video2'
+    
     if os.path.exists(video_device_2):
         camera_info_url_2 = Command([
             'echo "file://', launch_file_dir, '/', camera_name_2, '.yaml"'
@@ -47,6 +50,7 @@ def generate_launch_description():
                 'video_device': video_device_2,
                 'camera_info_url': camera_info_url_2,
                 'camera_name': camera_name_2,
+                'pixel_format': pixel_format
             }],
             remappings=[('image_raw', 'image_raw')]
         ))
