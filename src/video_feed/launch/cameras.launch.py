007273954952ea89a037765df4f2815236cfb2a5
Charlie <killiancharlie2@gmail.com>
2024-05-05 13:59:29 -0500

    2 more devices

--- src/video_feed/launch/cameras.launch.py
+++ src/video_feed/launch/cameras.launch.py
@@ -34,7 +34,7 @@ def generate_launch_description():
 
     # Camera 2
     camera_name_2 = 'camera2'
-    video_device_2 = '/dev/video4'  # Ensure this is correct
+    video_device_2 = '/dev/video4'
     
     if os.path.exists(video_device_2):
         ld.append(Node(
@@ -44,8 +44,46 @@ def generate_launch_description():
             namespace=camera_name_2,
             parameters=[{
                 'video_device': video_device_2,
-                'camera_info_url': camera_info_url,  # Make sure this URL is intended for camera2
+                'camera_info_url': camera_info_url,
                 'camera_name': camera_name_2,
+                'pixel_format': pixel_format,
+            }],
+            remappings=[('image_raw', 'image_raw')]
+        ))
+    
+     # Camera 3
+    camera_name_3 = 'camera3'
+    video_device_3 = '/dev/video8'  # Ensure this is correct
+    
+    if os.path.exists(video_device_3):
+        ld.append(Node(
+            package='usb_cam',
+            executable='usb_cam_node_exe',
+            name=camera_name_3,
+            namespace=camera_name_3,
+            parameters=[{
+                'video_device': video_device_3,
+                'camera_info_url': camera_info_url,
+                'camera_name': camera_name_3,
+                'pixel_format': pixel_format,
+            }],
+            remappings=[('image_raw', 'image_raw')]
+        ))
+
+    # Camera 4
+    camera_name_4 = 'camera4'
+    video_device_4 = '/dev/video12'  # Ensure this is correct
+    
+    if os.path.exists(video_device_4):
+        ld.append(Node(
+            package='usb_cam',
+            executable='usb_cam_node_exe',
+            name=camera_name_4,
+            namespace=camera_name_4,
+            parameters=[{
+                'video_device': video_device_4,
+                'camera_info_url': camera_info_url,  
+                'camera_name': camera_name_4,
                 'pixel_format': pixel_format,
             }],
             remappings=[('image_raw', 'image_raw')]
