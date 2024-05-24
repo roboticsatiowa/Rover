e1e336b99afd5f663056f368f778f0389a77586a
Ethan Holter <holterethan@gmail.com>
2024-05-23 20:28:07 -0500

    using fastdds discovery server

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,6 +4,7 @@ source /opt/ros/humble/setup.bash
 source /home/robotics/Rover/install/setup.bash
 
 sudo chmod 777 /dev/video*
+fastdds discovery --server-id 0 &
 ros2 launch rover_launch launch_all.launch.py
 
 exec "$@"
