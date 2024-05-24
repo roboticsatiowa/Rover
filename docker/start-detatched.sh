3e3053cfaa6849215870da531ffce4224cefc443
Ethan Holter <holterethan@gmail.com>
2024-05-23 22:02:59 -0500

    changed image name

--- docker/start-detatched.sh
+++ docker/start-detatched.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR=$( cd "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "${SCRIPT_DIR}"/..
 
 # TODO validate that the image has been built
-if ( ! docker image inspect rover:latest &> /dev/null); then
+if ( ! docker image inspect ethanholter/uirover:latest &> /dev/null); then
     echo "Image rover:latest not found. Please build the image first."
     exit 1
 fi
