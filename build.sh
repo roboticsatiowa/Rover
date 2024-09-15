4d89839982137a59cd97fccaf3227fc807cdeadf
Ethan <holterethan@gmail.com>
2024-09-14 23:11:39 -0500

    fixed line ending

--- build.sh
+++ build.sh
@@ -12,11 +12,11 @@ if ! command -v docker &> /dev/null; then
     exit 1
 fi
 
-docker build . -f docker/build.Dockerfile -t build-base:latest
-
+docker build . -f docker/build.Dockerfile -t build-base:latest 
 docker compose -f docker/compose.yml build
+
 echo "Rover built successfully"
-# echo 
-# echo "Pushing Rover files..."
-# docker compose -f docker/compose.yml push --quiet || echo "Error: Push failed"
-# echo "Rover pushed successfully"
+echo 
+echo "Pushing Rover files..."
+docker compose -f docker/compose.yml push --quiet
+echo "Rover pushed successfully"
