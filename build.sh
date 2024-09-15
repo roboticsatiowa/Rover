7ffa0945ec178c96c09df9dd332d323f1375a3cb
Ethan <holterethan@gmail.com>
2024-09-15 00:10:11 -0500

    removed space from name

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ if ! command -v docker &> /dev/null; then
 fi
 
 docker build . -f docker/build.Dockerfile -t build-base:latest 
-docker compose -f docker/compose.yml build
+docker compose -f docker/compose.yml build --no-cache
 
 echo "Rover built successfully"
 echo 
