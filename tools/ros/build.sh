55fb221d869e6dad005f6f5f60fac5fa3a0ff8b4
Ethan Holter <holterethan@gmail.com>
2024-11-20 23:12:47 -0600

    basics for gazebo simulation

--- tools/ros/build.sh
+++ tools/ros/build.sh
@@ -5,4 +5,3 @@ SCRIPT_DIR=$( cd "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 rm -rf $SCRIPT_DIR/../../install $SCRIPT_DIR/../../build
 (cd $SCRIPT_DIR/../.. && colcon build --symlink-install)
-source $SCRIPT_DIR/../../install/setup.bash
