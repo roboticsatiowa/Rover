73aa0a173f24200ba0e248ea88c1975e63f734d9
Ethan Holter <holterethan@gmail.com>
2024-11-21 21:34:01 -0600

    gazebo world change + build script update

--- tools/ros/build.sh
+++ tools/ros/build.sh
@@ -3,5 +3,4 @@ set -eo pipefail
 
 SCRIPT_DIR=$( cd "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-rm -rf $SCRIPT_DIR/../../install $SCRIPT_DIR/../../build
 (cd $SCRIPT_DIR/../.. && colcon build --symlink-install)
