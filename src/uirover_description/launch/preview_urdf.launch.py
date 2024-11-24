b3281753c646da78e7695eda241ce78136145f3a
Ethan Holter <holterethan@gmail.com>
2024-11-23 22:30:56 -0600

    updates to the rover urdf to work with gazebo and generally just be better

--- src/uirover_description/launch/preview_urdf.launch.py
+++ src/uirover_description/launch/preview_urdf.launch.py
@@ -4,8 +4,8 @@ import launch_ros
 import os
 
 def generate_launch_description():
-    pkg_share = launch_ros.substitutions.FindPackageShare(package='uiuirover_description').find('uiuirover_description')
-    default_model_path = os.path.join(pkg_share, 'src/description.urdf')
+    pkg_share = launch_ros.substitutions.FindPackageShare(package='uirover_description').find('uirover_description')
+    default_model_path = os.path.join(pkg_share, 'urdf/uirover.urdf.xacro')
     default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
 
     robot_state_publisher_node = launch_ros.actions.Node(
