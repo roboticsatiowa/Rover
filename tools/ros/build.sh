ad2d39572058e888f6348fe8b7fc6f2fa1a123bc
Ethan Holter <holterethan@gmail.com>
2024-11-24 13:52:40 -0600

    removed annoying warnings and desktop notification from build script

--- tools/ros/build.sh
+++ tools/ros/build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-SCRIPT_DIR=$( cd "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+WS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." &> /dev/null && pwd )" # workspace directory
 
-(cd $SCRIPT_DIR/../.. && colcon build --symlink-install)
+(cd $WS_DIR && colcon build --symlink-install --event-handlers desktop_notification- console_stderr-)
+
+echo
+echo "Stderr has been surpressed. Complete logs can be found at ${WS_DIR}/log/latest/events.log"
