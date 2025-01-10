825c996ee5c9995bdbcaa25353ae1aa1159ed9e5
Ethan Holter <holterethan@gmail.com>
2025-01-10 11:32:13 -0600

    zenoh bridge dependency

--- tools/ros/install_dependencies.sh
+++ tools/ros/install_dependencies.sh
@@ -3,6 +3,11 @@ set -eo pipefail
 
 WS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." &> /dev/null && pwd )" # workspace directory
 
+
+
+if ! grep -q "https://download.eclipse.org/zenoh/debian-repo/" /etc/apt/sources.list; then
+    echo "deb [trusted=yes] https://download.eclipse.org/zenoh/debian-repo/ /" | sudo tee -a /etc/apt/sources.list > /dev/null
+fi
 sudo apt update
 
 if [ ! -d /etc/ros/rosdep/sources.list.d ]; then
@@ -13,6 +18,7 @@ fi
 rosdep update --rosdistro jazzy
 rosdep install --from-paths $WS_DIR/src --ignore-src -y
 
+
 # other dependencies
-sudo apt install -y python3-colcon-common-extensions python3-colcon-clean 
+sudo apt install -y python3-colcon-common-extensions python3-colcon-clean zenoh-bridge-dds
 sudo snap install foxglove-studio
