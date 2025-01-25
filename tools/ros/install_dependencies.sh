9a0885704940b0ae50b9565df1857a5c5a94b85b
Ethan Holter <holterethan@gmail.com>
2025-01-25 15:51:20 -0600

    replaced compiled dependency with binary package

--- tools/ros/install_dependencies.sh
+++ tools/ros/install_dependencies.sh
@@ -9,7 +9,6 @@ if ! grep -q "https://download.eclipse.org/zenoh/debian-repo/" /etc/apt/sources.
     echo "deb [trusted=yes] https://download.eclipse.org/zenoh/debian-repo/ /" | sudo tee -a /etc/apt/sources.list > /dev/null
 fi
 sudo apt update
-sudo apt full-upgrade -y
 
 if [ ! -d /etc/ros/rosdep/sources.list.d ]; then
     sudo apt install -y python3-rosdep
@@ -22,9 +21,6 @@ rosdep install --from-paths "${WS_DIR}/src" --ignore-src -y
 
 # other dependencies
 sudo apt install -y python3-colcon-common-extensions python3-colcon-clean libopencv-dev zenoh-bridge-ros2dds
-
-# GStreamer
-# sudo apt install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
 
 # Foxglove Studio
 if [ ! -f /usr/bin/foxglove-studio ]; then
