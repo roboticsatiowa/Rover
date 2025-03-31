cdc638e5cb24b60899a3ee205942626f1cf203cd
Ethan Holter <holterethan@gmail.com>
2025-03-30 21:34:44 -0500

    added some missing dependencies

--- tools/ros/install_dependencies.sh
+++ tools/ros/install_dependencies.sh
@@ -18,13 +18,12 @@ fi
 rosdep update --rosdistro jazzy
 rosdep install --from-paths "${WS_DIR}/src" --ignore-src -y
 
-python3 -m pip install -U catkin_pkg
 
 # other dependencies
-sudo apt install -y python3-colcon-common-extensions python3-colcon-clean libopencv-dev zenoh-bridge-ros2dds
+sudo apt install -y python3-colcon-common-extensions python3-colcon-clean libopencv-dev zenoh-bridge-ros2dds python3-pip python3-catkin-pkg python3-venv
 
 # gstreamer
-apt-get install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
+sudo apt install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
 
 # PlatformIO
 if ! command -v platformio > /dev/null;  then
