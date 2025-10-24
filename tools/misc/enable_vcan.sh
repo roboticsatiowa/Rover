518088f2d0df8cf3104b5278e1298d22156ac5f6
Ethan Holter <holterethan@gmail.com>
2025-10-24 13:43:49 -0500

    tooling changes

--- tools/misc/enable_vcan.sh
+++ tools/misc/enable_vcan.sh
@@ -5,5 +5,16 @@
 # view outgoing CAN messages using `candump vcan0` from the can-utils package.
 
 sudo modprobe vcan
-sudo ip link add dev vcan0 type vcan
+echo "vcan kernel module enabled"
+
+if ! ip link | grep -q vcan; then
+    sudo ip link add dev vcan0 type vcan
+    echo "Created vcan0 network device"
+else
+    echo "vcan0 already exists"
+fi
+
 sudo ip link set up vcan0
+echo "vcan0 enabled"
+echo
+echo "Virtual CANbus network device has been enabled. Run 'ip link show vcan0' to check"
---
+++ tools/misc/setup_realtime.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# https://control.ros.org/rolling/doc/ros2_control/controller_manager/doc/userdoc.html
+# The normal linux kernel is optimized for computational throughput and therefore is not well suited for hardware control.
+# This script adds custom limits to /etc/security/limits.d/ to allow users of the 'realtime' group to increase their
+# CPU scheduling priority. This gives ROS2 control more consistent behavior and reduces jitter in control loops.
+# For the best possible performance a 'real-time' kernel should be installed. Nvidia provides guides for this 
+# here: https://docs.nvidia.com/jetson/archives/r36.2/DeveloperGuide/SD/Kernel/KernelCustomization.html
+#
+#
+# IMPORTANT: It may not be advisable to install a realtime kernel on your personal machine. Proceed with caution
+
+SCRIPT_DIR=$( cd "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+if ! cat "/etc/group" | grep -q realtime; then
+    sudo groupadd realtime
+    echo "Created realtime group"
+else
+    echo "realtime group already exists"
+fi
+
+if ! groups "$(whoami)" | grep -q realtime; then
+    sudo usermod -aG realtime "$(whoami)"
+    newgrp realtime
+    echo "Added $(whoami) to realtime group"
+else
+    echo "$(whoami) already in realtime group"
+fi
+
+if sudo cp "${SCRIPT_DIR}/99-uirover-rt-limits.conf" /etc/security/limits.d/; then
+    echo "Succesfully added security rules. Reboot to apply changes"
+else
+    echo "ERROR: Failed to add security rules"
+fi
+
