db963f284ea806d0984e2a32501d527f70e9c6ce
Ethan Holter <59928414+ethanholter@users.noreply.github.com>
2026-09-15 11:54:39 -0500

    Systemd & Udev (#98)
    * reorganized udev rules and scripts
    
    * added systemd service for rover

--- tools/misc/setup_udev.sh
+++ tools/misc/setup_udev.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 #
-# @(#)$Id$
-#
-# Author: Ethan Holter
 # This script symlinks the udev rules to the correct location
 # Udev rules allow various serial devices to have predictable names
 # without this, the serial devices will be assigned names like /dev/ttyACM0
@@ -19,18 +16,25 @@ fi
 
 SCRIPT_DIR=$( cd "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+UDEV_DIR="$SCRIPT_DIR"/../../udev
+
 # check if udev rules are already symlinked
-if [ ! -L /etc/udev/rules.d/99-uirover.rules ]; then
-  ln -s "$SCRIPT_DIR"/99-uirover.rules /etc/udev/rules.d/99-uirover.rules || exit 1
+if [ ! -L /etc/udev/rules.d/99-uirover-udev.rules ]; then
+  ln -s "$UDEV_DIR"/99-uirover-udev.rules /etc/udev/rules.d/99-uirover-udev.rules || exit 1
 fi
 
 if [ ! -L /etc/udev/rules.d/99-realsense-d4xx-mipi-dfu.rules ]; then
-  ln -s "$SCRIPT_DIR"/99-realsense-d4xx-mipi-dfu.rules /etc/udev/rules.d/99-realsense-d4xx-mipi-dfu.rules || exit 1
+  ln -s "$UDEV_DIR"/99-realsense-d4xx-mipi-dfu.rules /etc/udev/rules.d/99-realsense-d4xx-mipi-dfu.rules || exit 1
 fi
 
 if [ ! -L /etc/udev/rules.d/99-realsense-libusb.rules ]; then
-  ln -s "$SCRIPT_DIR"/99-realsense-libusb.rules /etc/udev/rules.d/99-realsense-libusb.rules || exit 1
+  ln -s "$UDEV_DIR"/99-realsense-libusb.rules /etc/udev/rules.d/99-realsense-libusb.rules || exit 1
 fi
+
+if [ ! -L /etc/security/limits.d/99-uirover-rt-limits.conf ]; then
+  ln -s "$UDEV_DIR"/99-uirover-rt-limits.conf /etc/security/limits.d/99-uirover-rt-limits.conf || exit 1
+fi
+
 udevadm control --reload-rules || exit 1
 
 echo "Success"
